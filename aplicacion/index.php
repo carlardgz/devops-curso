0862c79d7feafc3000a045d45edeadac03444fe0
Carla Rodriguez <carlardgz17@gmail.com>
2023-02-22 14:44:20 -0600

    cambios index

--- aplicacion/index.php
+++ aplicacion/index.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "148.213.3.5:3306";
-$database = "siceuc";
-$username = "root";
-$password = "secret";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
+echo "CURSO LOVEDEVOPS"
 ?>
